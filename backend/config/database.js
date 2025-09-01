ac9e248a521af9884c11a8ee9192ed518195df5c
CamiloAS04 <camiloalvarezsan04@gmail.com>
2025-09-01 18:51:57 -0500

    Add files via upload

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,26 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // This block will be used by Render
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  // This block will be used for local development
+  sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  });
+}
 
 module.exports = sequelize;
