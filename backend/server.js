d0365e3d5e032dadaf9fb5883ecc655747bb0e45
CamiloAS04 <camiloalvarezsan04@gmail.com>
2025-09-01 19:17:35 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,5 +17,5 @@ app.use("/api", rutas);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+  console.log(` Servidor corriendo en el puerto ${PORT}`);
 });
